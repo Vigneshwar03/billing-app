d7b3ba5179a5f4d92ed71dbc251c98a26b43ffa8
Vigneshwar03 <vigneshwar08v@gmail.com>
2026-06-19 12:10:54 +0530

    connect frontend to live backend

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api'
+ baseURL: 'https://billing-app-o5rw.onrender.com/api'
 });
 
 export const getCustomers = () => API.get('/customers');
